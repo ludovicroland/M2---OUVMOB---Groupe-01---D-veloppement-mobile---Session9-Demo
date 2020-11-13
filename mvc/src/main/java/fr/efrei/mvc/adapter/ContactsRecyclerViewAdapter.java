1a5ecb183d57b29a93cedcf6c70dbf008cbdd05e
Ludovic Roland <ludovic.c.roland@gmail.com>
2020-11-13 23:38:21 +0100

    Add the MVC sample

---
+++ mvc/src/main/java/fr/efrei/mvc/adapter/ContactsRecyclerViewAdapter.java
@@ -0,0 +1,76 @@
+package fr.efrei.mvc.adapter;
+
+import java.util.List;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import fr.efrei.mvc.R;
+import fr.efrei.mvc.model.Contact;
+
+/**
+ * @author Ludovic Roland
+ * @since 2020.11.13
+ */
+public class ContactsRecyclerViewAdapter
+    extends RecyclerView.Adapter<ContactsRecyclerViewAdapter.ViewHolder>
+{
+
+  public static final class ViewHolder
+      extends RecyclerView.ViewHolder
+  {
+
+    private final TextView name;
+
+    private final TextView number;
+
+    public ViewHolder(View view)
+    {
+      super(view);
+
+      name = view.findViewById(R.id.name);
+      number = view.findViewById(R.id.number);
+    }
+
+    public void update(Contact contact)
+    {
+      name.setText(contact.name);
+      number.setText(contact.phoneNumber);
+
+      itemView.setOnClickListener(v -> Toast.makeText(v.getContext(), contact.toString(), Toast.LENGTH_SHORT).show());
+    }
+
+  }
+
+  private final List<Contact> contacts;
+
+  public ContactsRecyclerViewAdapter(List<Contact> items)
+  {
+    contacts = items;
+  }
+
+  @Override
+  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+  {
+    final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+    return new ViewHolder(view);
+  }
+
+  @Override
+  public void onBindViewHolder(final ViewHolder holder, int position)
+  {
+    final Contact contact = contacts.get(position);
+    holder.update(contact);
+  }
+
+  @Override
+  public int getItemCount()
+  {
+    return contacts.size();
+  }
+}
---
+++ mvc/src/main/java/fr/efrei/mvc/controller/MainActivity.java
@@ -0,0 +1,53 @@
+package fr.efrei.mvc.controller;
+
+import java.util.List;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
+
+import fr.efrei.mvc.R;
+import fr.efrei.mvc.adapter.ContactsRecyclerViewAdapter;
+import fr.efrei.mvc.model.Contact;
+import fr.efrei.mvc.service.ContactApiService;
+
+/**
+ * @author Ludovic Roland
+ * @since 2020.11.13
+ */
+public class MainActivity
+    extends AppCompatActivity
+{
+
+  private List<Contact> contacts;
+
+  private RecyclerView recyclerView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+
+    recyclerView = findViewById(R.id.list_contacts);
+    recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+  }
+
+  @Override
+  protected void onResume()
+  {
+    super.onResume();
+
+    initList();
+  }
+
+  private void initList()
+  {
+    contacts = new ContactApiService().getContacts();
+    recyclerView.setAdapter(new ContactsRecyclerViewAdapter(contacts));
+  }
+
+}
