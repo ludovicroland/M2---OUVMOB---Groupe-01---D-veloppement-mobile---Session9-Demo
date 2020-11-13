e8011200787cf12c8df9f59721804ca121dac135
Ludovic Roland <ludovic.c.roland@gmail.com>
2020-11-13 23:07:04 +0100

    add the project structure with 3 modules

---
+++ mvc/src/main/java/fr/efrei/mvc/MainActivity.java
@@ -0,0 +1,17 @@
+package fr.efrei.mvc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity
+    extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
